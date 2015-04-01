40223f3e091ad1c969f0bbe67509270d9aed8534
Cristian González Fernández <uo231602@uniovi.es>
2015-04-01 23:35:26 +0200

    Añadida al proyecto la primera parte (obtención de las preguntas), arreglados los problemas que se generaban al pasar las pruebas de nuevo y solucionados todos los errores y warnings que se veían posibles por el momento

--- Game/Trivial 3_0/TrivialGame/src/conf/Conf.java
+++ Game/Trivial 3_0/TrivialGame/src/conf/Conf.java
@@ -2,8 +2,10 @@ package conf;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class Conf {
 
@@ -18,13 +20,13 @@ public class Conf {
 			f = new FileReader(ruta);
 			b = new BufferedReader(f);
 
-			String linea = b.readLine();
+			String linea = b.readLine().trim();
 			while (linea  != null) {
 
 				if (linea.startsWith("ID:")) {
 					nodes.add(linea.substring(3));
 				}
-				linea = b.readLine();
+				linea = b.readLine().trim();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -53,14 +55,14 @@ public class Conf {
 			f = new FileReader(ruta);
 			b = new BufferedReader(f);
 
-			String linea = b.readLine();
+			String linea = b.readLine().trim();
 			while (linea != null) {
 
 				if (linea.startsWith("ED:")) {
 					edges.add(linea.substring(3));
 				}
 				
-				linea = b.readLine();
+				linea = b.readLine().trim();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -75,5 +77,38 @@ public class Conf {
 		}
 
 		return edges;
+	}
+	
+private static final String CONF_FILE = "configuration.properties";
+	
+	private static Conf instance;
+	private Properties properties;
+
+	private Conf() {
+		properties = new Properties();
+		try {
+			properties.load(Conf.class.getClassLoader().getResourceAsStream(CONF_FILE));
+		} catch (IOException e) {
+			throw new RuntimeException("Propeties file can not be loaded", e);
+		}
+	}
+	
+	public static String get(String key) {
+		return getInstance().getProperty( key );
+	}
+
+	private String getProperty(String key) {
+		String value = properties.getProperty(key);
+		if (value == null) {
+			throw new RuntimeException("Property not found in config file");
+		}
+		return value;
+	}
+
+	private static Conf getInstance() {
+		if (instance == null) {
+			instance = new Conf();
+		}
+		return instance;
 	}
 }
--- Game/Trivial 3_0/TrivialGame/src/persistence/ScoreDao.java
+++ Game/Trivial 3_0/TrivialGame/src/persistence/ScoreDao.java
@@ -1,5 +1,4 @@
 package persistence;
 
 public interface ScoreDao {
-	
 }
