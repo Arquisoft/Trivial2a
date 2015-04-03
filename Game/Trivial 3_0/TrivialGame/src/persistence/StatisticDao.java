43a2f33436b52140e3d5afe3ac5528e48b26cfa9
María Piris García <merustineo@gmail.com>
2015-04-03 21:03:05 +0200

    Comentarios Statistics
    Comentarios aclarando el funcionamiento de los métodos de persistencia
    sobre las estadísticas del juego

--- Game/Trivial 3_0/TrivialGame/src/persistence/StatisticDao.java
+++ Game/Trivial 3_0/TrivialGame/src/persistence/StatisticDao.java
@@ -4,14 +4,51 @@ import java.util.Map;
 
 public interface StatisticDao {
 
+	/**
+	 * Método para obtener las estadísticas sobre deportes
+	 * @return Map<RespuestasTotalesDeportes, RespuestasCorrectasDeportes>
+	 */
 	Map<Integer, Integer> getStatisticSports();
+	
+	/**
+	 * Método para obtener las estadísticas sobre Entretenimiento
+	 * @return Map<RespuestasTotalesShows, RespuestasCorrectasShows>
+	 */
 	Map<Integer, Integer> getStatisticShows();
+	
+	/**
+	 * Método para obtener las estadísticas sobre Ciencia
+	 * @return Map<RespuestasTotalesCiencia, RespuestasCorrectasCiencia>
+	 */
 	Map<Integer, Integer> getStatisticScience();
+	
+	/**
+	 * Método para obtener las estadísticas sobre Arte y Literatura
+	 * @return Map<RespuestasTotalesArte, RespuestasCorrectasArte>
+	 */
 	Map<Integer, Integer> getStatisticArt();
+	
+	/**
+	 * método para obtener las estadísticas sobre Geografía
+	 * @return Map<RespuestasTotalesGeografia, RespuestasCorrectasGeografia>
+	 */
 	Map<Integer, Integer> getStatisticGeography();
+	
+	/**
+	 * Método para obtener las estadísticas sobre Historia
+	 * @return Map<RespuestasTotalesHistoria, RespuestasCorrectasHistoria>
+	 */
 	Map<Integer, Integer> getStatisticHistory();
 	
+	/**
+	 * Método para obtener las estadísticas clasificadas por temas
+	 * @return Map<Tema, RespuestasTotalesTema, RespuestasCorrectasTema>
+	 */
 	Map<String,Map<Integer,Integer>> getAllStatistics();
 	
+	/**
+	 * Método de utilidad para obtener el último ID de la tabla Statistics
+	 * @return int ID
+	 */
 	int getLastId();
 }
